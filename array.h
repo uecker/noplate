d8baf96dd8611bbb07be3a26289a7a47569049e1
Martin Uecker <ma.uecker@gmail.com>
2022-04-02 13:20:00 +0200

    fix macro var reuse

--- array.h
+++ array.h
@@ -13,13 +13,13 @@
 
 #define array_slice(x, start, end)				\
 (*({								\
-	auto __x = &(x);					\
+	auto __y = &(x);					\
    	ssize_t __start = (start);				\
 	ssize_t __end = (end);					\
 	CHECK(__start >= 0);					\
 	CHECK(__end >= __start);				\
-	CHECK((size_t)__end <= array_lengthof(*__x));		\
-	(array(typeof((*__x)[0]), __end - __start)*)&(*__x)[__start];	\
+	CHECK((size_t)__end <= array_lengthof(*__y));		\
+	(array(typeof((*__y)[0]), __end - __start)*)&(*__y)[__start];	\
 }))
 
 
