87c0f64742f8a3b06f3abfe91381a0e60b723788
Martin Uecker <ma.uecker@gmail.com>
2022-04-02 12:27:04 +0200

    peek/poke

---
+++ mem.h
@@ -0,0 +1,31 @@
+
+#ifndef _LDST_H
+#define _LDST_H	1
+
+#include "core.h"
+
+#define _LDST_MEMCPY	\
+	extern void *memcpy(void *dest, const void *src, typeof(sizeof(0)) n)
+
+#define peek(T, ptr) 				\
+(*({ 						\
+	typedef T _T;				\
+	enum { _N = sizeof(_T) };		\
+	char (*_ptr)[_N] = (ptr);		\
+	_LDST_MEMCPY;				\
+       	(_T*)memcpy(&(_T){ 0 }, _ptr, _N);	\
+}))
+
+#define poke(ptr, x)				\
+({						\
+ 	auto _x = (x);				\
+	enum { _N = sizeof(_x) };		\
+	char (*_ptr)[_N] = (ptr);		\
+	_LDST_MEMCPY;				\
+ 	memcpy(_ptr, &_x, _N);			\
+	_x;					\
+})
+
+
+#endif // _LDST_H
+
