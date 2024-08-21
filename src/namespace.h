c008eb676413be6e30583f20e4300e34cba09b09
Martin Uecker <uecker@tugraz.at>
2024-08-21 08:11:28 +0200

    namespace file

---
+++ src/namespace.h
@@ -0,0 +1,11 @@
+
+
+#define NAMESPACE_PREFIX(ns, x) ns ## _ ## x
+#define NAMESPACE_ENTRY(ns, x) typeof(NAMESPACE_PREFIX(ns, x)) *x;
+#define NAMESPACE_INIT(ns, x) .x = &NAMESPACE_PREFIX(ns, x),
+#define NAMESPACE_MAKE(S, N) \
+static const struct \
+{ S(N, NAMESPACE_ENTRY) } N = { S(N, NAMESPACE_INIT) }
+#define NAMESPACE_IMPORT(X, Y) const static typeof(X) Y = X
+
+
