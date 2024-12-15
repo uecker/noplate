3748be775cb5e6e90df8125f6ec416f764272b09
Martin Uecker <uecker@tugraz.at>
2024-12-15 10:05:40 +0100

    add maybe_just

--- src/maybe.h
+++ src/maybe.h
@@ -13,11 +13,10 @@
 #endif
 
 
-
 #define maybe_ok(T, x)	(maybe(T)){ .value = (x), .ok = true }
 #define maybe_fail(T)	(maybe(T)){ .value = (T){ 0 }, .ok = false }
 #define maybe_use(x)	({ auto __t = (x); CHECK(__t.ok); __t.value; })
-
+#define maybe_just(x)	({ auto __p = &(p); __p->ok ? &__p->value : (void*)0; })
 
 #endif	// __MAYBE_H
 
